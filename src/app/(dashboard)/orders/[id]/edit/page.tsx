4bb336f467fcbf748aed47010b54483cb910fe8c
Utilit1s <fran@puntocero.agency>
2025-05-05 16:14:01 -0400

    Created components for editOrder and use them in the view

--- src/app/(dashboard)/orders/[id]/edit/page.tsx
+++ src/app/(dashboard)/orders/[id]/edit/page.tsx
@@ -5,6 +5,8 @@ import { useParams, useRouter } from 'next/navigation';
 import Breadcrumb from '@/components/Breadcrumb';
 import Button from '@/components/Button';
 import Dropdown from '@/components/Dropdown';
+import OrderProductList from '@/components/OrderProductList';
+import OrderInfoPanel from '@/components/OrderInfoPanel';
 import { Colors } from '@/styles/styles';
 import { api } from '@/lib/sdkConfig';
 import { toast } from 'react-toastify';
@@ -18,7 +20,6 @@ import {
 } from '@pharmatech/sdk';
 import { useAuth } from '@/context/AuthContext';
 import Loading from '@/app/(dashboard)/loading';
-import Image from 'next/image';
 
 export default function EditOrderStatusPage() {
   const params = useParams();
@@ -109,7 +110,6 @@ export default function EditOrderStatusPage() {
   if (loading || !order) {
     return <Loading />;
   }
-
   const isDelivery = order.type === OrderType.DELIVERY;
 
   return (
@@ -147,138 +147,60 @@ export default function EditOrderStatusPage() {
         </div>
       </div>
 
-      <div className="mx-auto max-w-[904px] space-y-6 rounded-xl bg-white p-6 shadow-md">
-        <div className="flex flex-col gap-6 lg:flex-row">
-          {/* Panel izquierdo - 2/3 de ancho */}
-          <div className="w-full space-y-4 lg:w-2/3">
-            <div>
-              <h3 className="mb-2 font-semibold">Datos de Entrega</h3>
-              <p className="text-sm text-gray-600">
-                {order?.branch?.address ?? 'Dirección no disponible'}
-              </p>
-            </div>
-            <div>
-              <h3 className="mb-2 font-semibold">Datos del Cliente</h3>
-              <p className="text-sm text-gray-600">
-                <strong>Nombre:</strong>{' '}
-                {order?.branch?.name ?? 'Nombre no disponible'}
-              </p>
-              <p className="text-sm text-gray-600">
-                <strong>Teléfono:</strong>{' '}
-                {order?.branch?.name ?? 'Teléfono no disponible'}
-              </p>
-            </div>
-            <div>
-              <h3 className="mb-2 font-semibold">Datos del Repartidor</h3>
-              {selectedDeliveryUserId ? (
-                deliveryUsers
-                  .filter((user) => user.id === selectedDeliveryUserId)
-                  .map((user) => (
-                    <div key={user.id}>
-                      <p className="text-sm text-gray-600">
-                        <strong>Nombre:</strong> {user.firstName}{' '}
-                        {user.lastName}
-                      </p>
-                      <p className="text-sm text-gray-600">
-                        <strong>Teléfono:</strong>{' '}
-                        {user.phoneNumber ?? 'No disponible'}
-                      </p>
-                    </div>
-                  ))
-              ) : (
-                <p className="text-sm text-gray-600">
-                  No hay repartidor asignado.
-                </p>
-              )}
-            </div>
-          </div>
-
-          {/* Panel derecho - 1/3 de ancho */}
-          <div className="w-full space-y-4 lg:w-1/3">
-            <h3 className="mb-2 font-semibold">Productos</h3>
-            <div className="space-y-2">
-              {order.details.map((detail, idx) => (
-                <div
-                  key={idx}
-                  className="flex items-center space-x-4 rounded border p-2"
-                >
-                  <Image
-                    src={detail.productPresentation.product.images?.[0]?.url}
-                    alt="Producto"
-                    className="h-12 w-12 rounded object-cover"
-                    width={48}
-                    height={48}
-                  />
-                  <div className="flex-1">
-                    <p className="text-sm font-medium">
-                      {detail.productPresentation.product.name}
-                    </p>
-                    <p className="text-xs text-gray-500">
-                      {detail.productPresentation.presentation.name}
-                    </p>
-                  </div>
-                  <span className="font-semibold text-gray-700">
-                    x{detail.quantity}
-                  </span>
-                </div>
-              ))}
-              <p className="text-lg font-semibold text-gray-800">
-                Total: $
-                {order.details
-                  .reduce((total, detail) => {
-                    const price = detail.productPresentation.price || 0;
-                    return total + price * detail.quantity;
-                  }, 0)
-                  .toFixed(2)}
-              </p>
-            </div>
-          </div>
-        </div>
-
-        {/* Selectores debajo en 100% */}
-        <div className="mt-6 space-y-4">
-          <Dropdown
-            title="Estado de la Orden"
-            placeholder="Selecciona el estado"
-            width="100%"
-            selected={orderStatus}
-            onChange={(value) => setOrderStatus(value as OrderStatus)}
-            items={Object.values(OrderStatus).map((statusKey) => ({
-              label: statusKey,
-              value: statusKey,
-            }))}
+      <div className="mx-auto flex max-w-[904px] flex-col gap-6 lg:flex-row">
+        <div className="w-full space-y-6 lg:w-2/3">
+          <OrderInfoPanel
+            branch={order.branch}
+            selectedDeliveryUserId={selectedDeliveryUserId}
+            deliveryUsers={deliveryUsers}
           />
 
-          {isDelivery && (
-            <>
-              <Dropdown
-                title="Estado del Delivery"
-                placeholder="Selecciona el estado del delivery"
-                width="100%"
-                selected={deliveryStatus}
-                onChange={(value) =>
-                  setDeliveryStatus(value as OrderDeliveryStatus)
-                }
-                items={Object.values(OrderDeliveryStatus).map((statusKey) => ({
-                  label: statusKey,
-                  value: statusKey,
-                }))}
-              />
+          <div className="space-y-4 rounded-xl bg-white p-6 shadow-md">
+            <Dropdown
+              title="Estado de la Orden"
+              placeholder="Selecciona el estado"
+              width="100%"
+              selected={orderStatus}
+              onChange={(value) => setOrderStatus(value as OrderStatus)}
+              items={Object.values(OrderStatus).map((statusKey) => ({
+                label: statusKey,
+                value: statusKey,
+              }))}
+            />
 
-              <Dropdown
-                title="Asignar Repartidor"
-                placeholder="Selecciona un repartidor"
-                width="100%"
-                selected={selectedDeliveryUserId ?? ''}
-                onChange={(value) => setSelectedDeliveryUserId(value)}
-                items={deliveryUsers.map((user) => ({
-                  label: user.firstName + ' ' + user.lastName,
-                  value: user.id,
-                }))}
-              />
-            </>
-          )}
+            {isDelivery && (
+              <>
+                <Dropdown
+                  title="Estado del Delivery"
+                  placeholder="Selecciona el estado del delivery"
+                  width="100%"
+                  selected={deliveryStatus}
+                  onChange={(value) =>
+                    setDeliveryStatus(value as OrderDeliveryStatus)
+                  }
+                  items={Object.values(OrderDeliveryStatus).map(
+                    (statusKey) => ({
+                      label: statusKey,
+                      value: statusKey,
+                    }),
+                  )}
+                />
+                <Dropdown
+                  title="Asignar Repartidor"
+                  placeholder="Selecciona un repartidor"
+                  width="100%"
+                  selected={selectedDeliveryUserId ?? ''}
+                  onChange={(value) => setSelectedDeliveryUserId(value)}
+                  items={deliveryUsers.map((user) => ({
+                    label: user.firstName + ' ' + user.lastName,
+                    value: user.id,
+                  }))}
+                />
+              </>
+            )}
+          </div>
         </div>
+        <OrderProductList details={order.details} />
       </div>
     </>
   );
